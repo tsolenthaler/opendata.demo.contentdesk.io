731e0e88c7960654d453b1644b3ed35815733089
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-05-06 15:54:52 +0200

    Refactor environment variable handling for consistency and clarity in CI workflow and start.py

--- start.py
+++ start.py
@@ -1,6 +1,5 @@
 from os import getenv
 from dotenv import load_dotenv, find_dotenv
-import sys
 import contentdeskopendata
 import os
 
@@ -12,20 +11,13 @@ AKENEO_CLIENT_ID = getenv('AKENEO_CLIENT_ID')
 AKENEO_CLIENT_SECRET = getenv('AKENEO_CLIENT_SECRET')
 AKENEO_USERNAME = getenv('AKENEO_USERNAME')
 AKENEO_PASSWORD = getenv('AKENEO_PASSWORD')
-
 CDN_URL = getenv('CDN_URL')
 
 def main():
-    host = AKENEO_HOST
-    clientid = AKENEO_CLIENT_ID
-    secret = AKENEO_CLIENT_SECRET
-    user = AKENEO_USERNAME
-    passwd = AKENEO_PASSWORD
-    cdnurl = CDN_URL
     path = "/docs"
     projectPath = os.path.dirname(os.path.abspath(__file__))
     
-    contentdeskopendata.ContentdeskOpenData(host, clientid, secret, user, passwd, cdnurl, projectPath+path)
+    contentdeskopendata.ContentdeskOpenData(AKENEO_HOST, AKENEO_CLIENT_ID, AKENEO_CLIENT_SECRET, AKENEO_USERNAME, AKENEO_PASSWORD, CDN_URL, projectPath+path)
 
 if __name__ == '__main__':
     main()
