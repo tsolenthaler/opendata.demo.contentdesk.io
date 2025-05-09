93bd560d22a2117b4aac3717de815af4eb25a2f2
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-05-09 11:48:37 +0200

    Add additional environment variables to ContentdeskOpenData initialization for enhanced configuration

--- start.py
+++ start.py
@@ -13,11 +13,18 @@ AKENEO_USERNAME = getenv('AKENEO_USERNAME')
 AKENEO_PASSWORD = getenv('AKENEO_PASSWORD')
 CDN_URL = getenv('CDN_URL')
 
+APP_ORGANIZATION = getenv('APP_ORGANIZATION')
+APP_NAME = getenv('APP_NAME')
+APP_WEBSITE= getenv('APP_WEBSITE')
+APP_WEBSITE_API = getenv('APP_WEBSITE_API')
+APP_ORGANIZATION_WEBSITE = getenv('APP_ORGANIZATION_WEBSITE')
+APP_REGION = getenv('APP_REGION')
+
 def main():
     path = "/docs"
     projectPath = os.path.dirname(os.path.abspath(__file__))
     
-    contentdeskopendata.ContentdeskOpenData(AKENEO_HOST, AKENEO_CLIENT_ID, AKENEO_CLIENT_SECRET, AKENEO_USERNAME, AKENEO_PASSWORD, CDN_URL, projectPath+path)
+    contentdeskopendata.ContentdeskOpenData(AKENEO_HOST, AKENEO_CLIENT_ID, AKENEO_CLIENT_SECRET, AKENEO_USERNAME, AKENEO_PASSWORD, CDN_URL, projectPath+path, APP_ORGANIZATION, APP_NAME, APP_WEBSITE, APP_ORGANIZATION_WEBSITE, APP_REGION)
 
 if __name__ == '__main__':
     main()
